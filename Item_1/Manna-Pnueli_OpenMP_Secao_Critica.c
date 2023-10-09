68834ebc6c61ab4c1325f450f1db039a94ae499e
Enzo Oliveira <ero2003730@gmail.com>
2023-10-09 18:16:39 -0300

    Adicionando o primeiro arquivo do Item 1

---
+++ Item_1/Manna-Pnueli_OpenMP_Secao_Critica.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <omp.h>
+
+#define forever 100000000 // 10^8
+#define num_clients 2
+
+volatile int request = 0, respond = 0;
+volatile int sum = 0;
+
+void client_function(int i)
+{
+    int aux = 0;
+
+    while (aux < forever)
+    {
+        // Non-critical section (omitted)
+
+        // Pre-protocol
+        while (respond != i)
+        {
+            request = i;
+        }
+
+        // Critical section
+        int local = sum;
+        sleep(rand() % 2);
+        sum = local + 1;
+
+        printf("Final sum: %d\n", sum);
+
+        // Post-protocol
+        respond = 0;
+
+        aux++;
+    }
+}
+
+void server_function()
+{
+    int aux = 0;
+
+    while (aux < forever * num_clients)
+    {
+        // Await a request
+        while (request == 0)
+            continue;
+
+        // Grant access to the critical section
+        respond = request;
+
+        // Wait for the critical section to be vacated
+        while (respond != 0)
+            continue;
+
+        request = 0;
+        aux++;
+    }
+}
+
+int main()
+{
+#pragma omp parallel num_threads(num_clients + 1)
+    {
+        int tid = omp_get_thread_num();
+
+        if (tid == 0)
+        {
+            server_function();
+        }
+        else
+        {
+            client_function(tid);
+        }
+    }
+
+    return 0;
+}
